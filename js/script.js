cebed400f1219ed4c34cb49e948824a3209f6b19
josekamaumwas@yahoo.com <your-email@example.com>
2026-03-27 14:37:26 +0300

    Adding product image and qiuck links at footer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
 let slides = document.querySelectorAll(".hero-slider img");
 let current = 0;
 
+const messages = [
+    {
+        title: "High-Quality Fortified Maize Flour",
+        text: "Affordable, safe, and high quality maize flour."
+    },
+    {
+        title: "Supporting Local Farmers",
+        text: "We source maize directly from small-scale farmers."
+    },
+    {
+        title: "Zero Waste Production",
+        text: "All by-products are converted into animal feed."
+    }
+];
+
 function changeSlide() {
     slides[current].classList.remove("active");
     current = (current + 1) % slides.length;
     slides[current].classList.add("active");
+
+    document.getElementById("hero-title").textContent = messages[current].title;
+    document.getElementById("hero-text").textContent = messages[current].text; 
 }
 
-setInterval(changeSlide, 3000);
+setInterval(changeSlide, 5000);
